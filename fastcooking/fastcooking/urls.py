7dfe4a75000c69c508ffe68e4a2f9f861260a780
artemkorep <artemkorepanov8@gmail.com>
2024-05-19 01:58:39 +0300

    Create ML

--- fastcooking/fastcooking/urls.py
+++ fastcooking/fastcooking/urls.py
@@ -4,12 +4,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from recipes.views import *
 
+
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('api/v1/recipe/', RecipesAPIList.as_view()),
                   path('api/v1/recipe/<int:pk>/', RecipesAPIUpdate.as_view()),
                   path('api/v1/recipedelete/<int:pk>/', RecipesAPIDestroy.as_view()),
                   path('media/recipe_images/', RecipeImageView.as_view(), name='get_image'),
+                  path('api/ml_images/', include('ml.urls')),
                   path('api/v1/auth/', include('djoser.urls')),
                   re_path(r'^auth/', include('djoser.urls.authtoken')),
                   path('search/', RecipesViewSet.as_view({'get': 'list'}), name='recipes-list'),
---
+++ fastcooking/ml/views.py
@@ -0,0 +1,31 @@
+# myapp/views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.conf import settings
+from inference_sdk import InferenceHTTPClient
+import tempfile
+from PIL import Image
+
+class PredictView(APIView):
+    def post(self, request):
+        file = request.FILES['image']
+
+        # Сохранение загруженного изображения во временный файл
+        with tempfile.NamedTemporaryFile(delete=False) as temp_image:
+            for chunk in file.chunks():
+                temp_image.write(chunk)
+            temp_image_path = temp_image.name
+
+        # Инициализация клиента
+        client = InferenceHTTPClient(
+            api_url=settings.ROBOFLOW_API_URL,
+            api_key=settings.ROBOFLOW_API_KEY
+        )
+
+        # Предсказание
+        try:
+            result = client.infer(temp_image_path, model_id="eat-69w8e/3")
+            return Response(result, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
