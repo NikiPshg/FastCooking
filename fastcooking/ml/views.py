daef606787e3431562d5c7f392f35e358a425225
artemkorep <artemkorepanov8@gmail.com>
2024-05-30 02:40:45 +0300

    Edit ML
    Translation and comparison

--- fastcooking/ml/views.py
+++ fastcooking/ml/views.py
@@ -7,6 +7,10 @@ from inference_sdk import InferenceHTTPClient
 import tempfile
 from PIL import Image
 
+from recipes.models import Recipes  # Импортируем модель
+from recipes.serializers import RecipeSerializerML  # Импортируем сериализатор
+from googletrans import Translator
+
 
 class PredictView(APIView):
     def post(self, request):
@@ -27,6 +31,41 @@ class PredictView(APIView):
         # Предсказание
         try:
             result = client.infer(temp_image_path, model_id="eat-69w8e/3")
-            return Response(result, status=status.HTTP_200_OK)
+
+            # Извлечение уникальных классов из предсказаний
+            unique_classes = list(set(prediction['class'] for prediction in result['predictions']))
+
+            # Перевод уникальных классов на русский
+            translator = Translator()
+            translated_classes = [translator.translate(cls, src='en', dest='ru').text for cls in unique_classes]
+
+            # Поиск рецептов, которые содержат все совпадающие ингредиенты
+            matching_recipes = []
+            for recipe in Recipes.objects.all():
+                recipe_ingredients = set(recipe.ingr.split(','))
+                if set(translated_classes).issubset(recipe_ingredients):
+                    matching_recipes.append(recipe)
+
+            # Сериализация рецептов для ответа
+            serialized_recipes = RecipeSerializerML(matching_recipes, many=True)
+
+            return Response({"classes": translated_classes, "matching_recipes": serialized_recipes.data},
+                            # после убрать translated_classes
+                            status=status.HTTP_200_OK)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        # # выводит классы
+        # try:
+        #     result = client.infer(temp_image_path, model_id="eat-69w8e/3")
+        #
+        #     # Извлечение уникальных классов из предсказаний
+        #     unique_classes = list(set(prediction['class'] for prediction in result['predictions']))
+        #
+        #     # Перевод уникальных классов на русский
+        #     translator = Translator()
+        #     translated_classes = [translator.translate(cls, src='en', dest='ru').text for cls in unique_classes]
+        #
+        #     return Response({"classes": translated_classes}, status=status.HTTP_200_OK)
+        # except Exception as e:
+        #     return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
