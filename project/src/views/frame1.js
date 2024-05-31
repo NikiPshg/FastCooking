f6a6d3cc766aa6ff5cf41d0e1174475c2a3a778e
ProstoArtems <105981938+ProstoArtems@users.noreply.github.com>
2024-05-31 07:00:15 +0300

    7-th commit
    Добавил возможность загружать фото на сайт для дальнейшей его обработки нашей моделью

--- project/src/views/frame1.js
+++ project/src/views/frame1.js
@@ -4,33 +4,38 @@ import axios from 'axios';
 const src = "https://content.guardianapis.com/search?q=debate&tag=politics/politics&from-date=2014-01-01&api-key=test"
 function Frame1() {
 
-    const [articles, setArticles] = useState([]);
-    useEffect(() =>{
-        axios
-            .get(src)
-            .then(data => {
-                setArticles(data.data.response.results)
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileChange = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
+
+    const handleUpload = () => {
+        const formData = new FormData();
+        formData.append('file', selectedFile);
+
+        axios.put('http://127.0.0.1:8000/api/v1/media/recipe_images/', formData)
+            .then(response => {
+                console.log('File uploaded successfully:', response.data);
             })
-    }, []);
-  return (
-      <div className="frame1-container">
-          {articles.map(article =>{
-              return(
-                  <p>{article.webTitle}</p>
-              )
-          })}
-          <div className="frame1-frame1">
-              <img src="/fluentmdl2eatdrink6056-2zx.svg"
-                   alt="fluentmdl2eatdrink6056"
-                   className="frame1-fluentmdl2eatdrink"
-              />
-              <span className="frame1-text">
+            .catch(error => {
+                console.error('Error uploading file:', error);
+            });
+    };
+    return (
+        <div className="frame1-container">
+            <div className="frame1-frame1">
+                <img src="/fluentmdl2eatdrink6056-2zx.svg"
+                     alt="fluentmdl2eatdrink6056"
+                     className="frame1-fluentmdl2eatdrink"
+                />
+                <span className="frame1-text">
           <span>Рецепты FastCooking</span>
         </span>
-              <span className="frame1-text02">
+                <span className="frame1-text02">
             Наш сайт - ваш личный ассистент в кулинарии!
         </span>
-              <span className="frame1-text06">
+                <span className="frame1-text06">
           <span>Просто сфотографируйте продукты, которые у вас есть,</span>
           <br></br>
           <span>и он мгновенно подберет для вас самые вкусные</span>
@@ -41,23 +46,23 @@ function Frame1() {
           <br></br>
           <span>с нашим умным помощником!</span>
         </span>
-              <span className="frame1-text16">
+                <span className="frame1-text16">
           <span>Отзывы наших клиентов</span>
         </span>
-              <img
-                  src="/ellipse16057-jeks-200h.png"
-                  alt="Ellipse16057"
-                  className="frame1-ellipse1"
-              />
-              <span className="frame1-text18">
+                <img
+                    src="/ellipse16057-jeks-200h.png"
+                    alt="Ellipse16057"
+                    className="frame1-ellipse1"
+                />
+                <span className="frame1-text18">
           <span>Матвей Лень</span>
         </span>
-              <img
-                  src="/rectangle56057-43ju-400h.png"
-                  alt="Rectangle56057"
-                  className="frame1-rectangle5"
-              />
-              <span className="frame1-text20">
+                <img
+                    src="/rectangle56057-43ju-400h.png"
+                    alt="Rectangle56057"
+                    className="frame1-rectangle5"
+                />
+                <span className="frame1-text20">
           <span>Сайт очень удобный, подобрал своей</span>
           <br></br>
           <span>
@@ -82,62 +87,62 @@ function Frame1() {
           <br></br>
           <span>Спасибо!</span>
         </span>
-              <div className="stars1">
-                  <img
-                      src="/star13.png"
-                      className="stars1-star1"
-                  />
-                  <img
-                      src="/star13.png"
-                      className="stars1-star2"
-                  />
-                  <img
-                      src="/star13.png"
-                      className="stars1-star3"
-                  />
-                  <img
-                      src="/star13.png"
-                      className="stars1-star4"
-                  />
-                  <img
-                      src="/star13.png"
-                      className="stars1-star5"
-                  />
-              </div>
+                <div className="stars1">
+                    <img
+                        src="/star13.png"
+                        className="stars1-star1"
+                    />
+                    <img
+                        src="/star13.png"
+                        className="stars1-star2"
+                    />
+                    <img
+                        src="/star13.png"
+                        className="stars1-star3"
+                    />
+                    <img
+                        src="/star13.png"
+                        className="stars1-star4"
+                    />
+                    <img
+                        src="/star13.png"
+                        className="stars1-star5"
+                    />
+                </div>
 
-              <div className="stars2">
-                  <img
-                      src="/star13.png"
-                      className="stars2-star1"
-                  />
-                  <img
-                      src="/star13.png"
-                      className="stars2-star2"
-                  />
-                  <img
-                      src="/star13.png"
-                      className="stars2-star3"
-                  />
-                  <img
-                      src="/star13.png"
-                      className="stars2-star4"
-                  />
-                  <img
-                      src="/star13.png"
-                      className="stars2-star5"
-                  />
-              </div>
-              <img
-                  src="/vector16058-m0gc.svg"
-                  alt="Vector16058"
-                  className="frame1-vector1"
-              />
-              <img
-                  src="/rectangle66058-xxw8-400h.png"
-                  alt="Rectangle66058"
-                  className="frame1-rectangle6"
-              />
-              <span className="frame1-text30">
+                <div className="stars2">
+                    <img
+                        src="/star13.png"
+                        className="stars2-star1"
+                    />
+                    <img
+                        src="/star13.png"
+                        className="stars2-star2"
+                    />
+                    <img
+                        src="/star13.png"
+                        className="stars2-star3"
+                    />
+                    <img
+                        src="/star13.png"
+                        className="stars2-star4"
+                    />
+                    <img
+                        src="/star13.png"
+                        className="stars2-star5"
+                    />
+                </div>
+                <img
+                    src="/vector16058-m0gc.svg"
+                    alt="Vector16058"
+                    className="frame1-vector1"
+                />
+                <img
+                    src="/rectangle66058-xxw8-400h.png"
+                    alt="Rectangle66058"
+                    className="frame1-rectangle6"
+                />
+                <span className="frame1-text30">
           <span>
             Отличный сайт для тех, кто не любит
             <span
@@ -162,99 +167,99 @@ function Frame1() {
           <br></br>
           <span>подходящие рецепты. Удобно и быстро</span>
         </span>
-              <span className="frame1-text40">
+                <span className="frame1-text40">
           <span></span>
           <br></br>
           <span></span>
         </span>
 
-              <img
-                  src="/vector26059-fang.svg"
-                  alt="Vector26059"
-                  className="frame1-vector2"
-              />
-              <img
-                  src="/ellipse26059-k1d-200h.png"
-                  alt="Ellipse26059"
-                  className="frame1-ellipse2"
-              />
-              <span className="frame1-text44">
+                <img
+                    src="/vector26059-fang.svg"
+                    alt="Vector26059"
+                    className="frame1-vector2"
+                />
+                <img
+                    src="/ellipse26059-k1d-200h.png"
+                    alt="Ellipse26059"
+                    className="frame1-ellipse2"
+                />
+                <span className="frame1-text44">
           <span>Никифорова Екатерина</span>
         </span>
-              <a href="http://localhost:3000/meat">
-              <img
-                  src="/rectangle166059-9d09-500h.png"
-                  alt="Rectangle166059"
-                  className="frame1-rectangle16"
-              />
-              </a>
-              <span className="frame1-text46">
+                <a href="http://localhost:3000/meat">
+                    <img
+                        src="/rectangle166059-9d09-500h.png"
+                        alt="Rectangle166059"
+                        className="frame1-rectangle16"
+                    />
+                </a>
+                <span className="frame1-text46">
           <span>Мясо</span>
         </span>
-              <img
-                  src="/rectangle17button6059-e5ne-500h.png"
-                  alt="Rectangle17button6059"
-                  className="frame1-rectangle17button"
-              />
+                <img
+                    src="/rectangle17button6059-e5ne-500h.png"
+                    alt="Rectangle17button6059"
+                    className="frame1-rectangle17button"
+                />
 
-              <div className="header">
+                <div className="header">
           <span className="frame1-text54">
           <span className="frame1-text55">Fast</span>
           <br></br>
           <span className="frame1-text57"></span>
           <span>Cooking</span>
         </span>
-                  <img
-                      src="star17.svg"
-                      alt="Star16051"
-                      className="frame1-star1"
-                  />
-                  <input
-                      className="frame1-rectangle13"
-                      type="search"
-                      placeholder="Выбрать рецепт"
-                  />
-                  <a className="frame1-text48" href="login">
-                      Войти
-                  </a>
-                  <a className="frame1-text50" href="reg">
-                      Регистрация
-                  </a>
-                  <img
-                      src="Lupa.png"
-                      alt="Ellipse46051"
-                      className="frame1-ellipse4"
-                  />
-                  <a className="frame1-text59" href="#About1">
-                      О сайте
-                  </a>
-              </div>
+                    <img
+                        src="star17.svg"
+                        alt="Star16051"
+                        className="frame1-star1"
+                    />
+                    <input
+                        className="frame1-rectangle13"
+                        type="search"
+                        placeholder="Выбрать рецепт"
+                    />
+                    <a className="frame1-text48" href="login">
+                        Войти
+                    </a>
+                    <a className="frame1-text50" href="reg">
+                        Регистрация
+                    </a>
+                    <img
+                        src="Lupa.png"
+                        alt="Ellipse46051"
+                        className="frame1-ellipse4"
+                    />
+                    <a className="frame1-text59" href="#About1">
+                        О сайте
+                    </a>
+                </div>
 
-              <img
-                  src="/line16051-ppyd.svg"
-                  alt="Line16051"
-                  className="frame1-line1"
-              />
+                <img
+                    src="/line16051-ppyd.svg"
+                    alt="Line16051"
+                    className="frame1-line1"
+                />
 
 
-              <img
-                  src="/rectangle196051-kprf-200h.png"
-                  alt="Rectangle196051"
-                  className="frame1-rectangle19"
-              />
-              <ul className="widgets">
-                  <a href="tg" className="fa fa-telegram"></a>
-                  <a href="vk" className="fa fa-vk"></a>
-              </ul>
-              <img
-                  src="/biccircle6051-pxnv.svg"
-                  alt="biccircle6051"
-                  className="frame1-biccircle"
-              />
-              <span className="frame1-text61">
+                <img
+                    src="/rectangle196051-kprf-200h.png"
+                    alt="Rectangle196051"
+                    className="frame1-rectangle19"
+                />
+                <ul className="widgets">
+                    <a href="tg" className="fa fa-telegram"></a>
+                    <a href="vk" className="fa fa-vk"></a>
+                </ul>
+                <img
+                    src="/biccircle6051-pxnv.svg"
+                    alt="biccircle6051"
+                    className="frame1-biccircle"
+                />
+                <span className="frame1-text61">
           <span>FastCooking, 2024</span>
         </span>
-              <span className="frame1-text63">
+                <span className="frame1-text63">
           <span>
             Если возникли какие-то
             <span
@@ -266,37 +271,39 @@ function Frame1() {
           <br></br>
           <span>проблемы, свяжитесь с нами</span>
         </span>
-              <span className="frame1-text67">
+                <span className="frame1-text67">
           <span>Вверх</span>
         </span>
-              <a className="link" href="#">
-              </a>
-              <img
-                  src="arrow.png"
-                  className="arrow"
-              />
-              <span className="frame1-text69">
+                <a className="link" href="#">
+                </a>
+                <img
+                    src="arrow.png"
+                    className="arrow"
+                />
+                <span className="frame1-text69">
                 <span className="frame1-text691">Здоровое</span>
                 <span className="frame1-text692">питание</span>
             </span>
-              <input
-                  type="file"
-                  className="frame1-rectangle15"
-              />
-              <span className="frame1-text71">
+                <input
+                    type="file"
+                    className="frame1-rectangle15"
+                    onChange={handleFileChange}
+                />
+                <button onClick={handleUpload}>Upload</button>
+                <span className="frame1-text71">
           <span>Загрузи фото продуктов</span>
           <br></br>
           <span>и получи рецепт мечты!</span>
         </span>
-              <img
-                  src="/vector6051-akij.svg"
-                  alt="Vector6051"
-                  className="frame1-vector"
-              />
-              <div className="about" id="About1"></div>
-          </div>
-      </div>
-  )
+                <img
+                    src="/vector6051-akij.svg"
+                    alt="Vector6051"
+                    className="frame1-vector"
+                />
+                <div className="about" id="About1"></div>
+            </div>
+        </div>
+    )
 }
 
 export default Frame1
--- project/src/views/meat.js
+++ project/src/views/meat.js
@@ -15,7 +15,7 @@ const Meat = (props) => {
             .get("http://127.0.0.1:8000/api/v1/urls/")
             .then((response) => {
                 setArticles(response.data.image_urls); // Убедитесь, что данные находятся в response.data
-                console.log(response.data.image_urls);
+                console.log(response.data);
             })
             .catch((error) => {
                 console.error("Ошибка при получении данных:", error);
@@ -32,9 +32,34 @@ const Meat = (props) => {
                     @import url('https://fonts.cdnfonts.com/css/days-one');
                 </style>
             </Helmet>
+
+            <div className="header">
+                <span className="frame1-text54">
+                <span className="frame1-text55">Fast</span>
+                <br></br>
+                <span className="frame1-text57"></span>
+                <span>Cooking</span>
+            </span>
+                <img
+                    src="star17.svg"
+                    alt="Star16051"
+                    className="frame1-star1"
+                />
+                <input
+                    className="frame1-rectangle131"
+                    type="search"
+                    placeholder="Выбрать рецепт"
+                />
+                <img
+                    src="Lupa.png"
+                    alt="Ellipse46051"
+                    className="frame1-ellipse41"
+                />
+            </div>
+
             <form className="text-form">
                 {articles.map((url, index) => (
-                    <img key={index} src={url} alt={index}/>
+                    <img key={index} src={url} alt={index} className="image"/>
                 ))}
             </form>
         </div>
